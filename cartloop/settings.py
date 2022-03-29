ea8f40281ab9b7ea0819ce4dd840eeb1d10a8be5
Ayuo Buba <ayobuba@gmail.com>
2022-03-29 15:03:46 +0100

    reenabled secrets

--- cartloop/settings.py
+++ cartloop/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -21,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = 'us!h@wyk@eu^*751$#vc^l7(2g)6v-&&22%46qlybcw@qmyy+l'
 # SECRET_KEY = 'us!h@wyk@eu^*751$#vc^l7(2g)6v-&&22%46qlybcw@qmyy+l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
